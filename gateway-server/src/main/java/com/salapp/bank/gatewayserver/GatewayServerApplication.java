0cfe9812d9d033e728fa70b3203d8ffca5865d08
Stainley Lebron <stainley.lebron@gmail.com>
2024-10-13 17:56:36 -0400

    gateway

--- gateway-server/src/main/java/com/salapp/bank/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/salapp/bank/gatewayserver/GatewayServerApplication.java
@@ -2,12 +2,24 @@ package com.salapp.bank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/users/**").uri("lb://user-service"))
+                .route("account-service", r -> r.path("/accounts/**").uri("lb://account-service"))
+                .route("transaction-service", r -> r.path("/transactions/**").uri("lb://transaction-service"))
+                .build();
     }
 
 }
