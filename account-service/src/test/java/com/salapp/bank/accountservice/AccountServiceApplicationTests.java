6725f338aa20052726e2d7b5a41cd21befcf99ba
Stainley Lebron <stainley.lebron@gmail.com>
2024-10-22 15:43:27 -0400

    configuration

--- account-service/src/test/java/com/salapp/bank/accountservice/AccountServiceApplicationTests.java
+++ account-service/src/test/java/com/salapp/bank/accountservice/AccountServiceApplicationTests.java
@@ -1,12 +1,19 @@
 package com.salapp.bank.accountservice;
 
+import com.netflix.discovery.EurekaClientConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@EnableAutoConfiguration
 class AccountServiceApplicationTests {
 
     @Test
